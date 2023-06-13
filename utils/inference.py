873c48f9d603a7cd7f1c6481c7ef42fbafa18279
NguyenHieu201 <hieu.nguyenminh2012001@gmail.com>
2023-06-14 00:24:18 +0700

    fix code and first run

--- utils/inference.py
+++ utils/inference.py
@@ -1,25 +1,24 @@
 import torch
 import torchvision
 import numpy as np
-import cv2
+import cv2 as cv
 
 
 def inference(model, img, image_size=(360, 640)):
-    h, w = img.shape[:2]
-    frames_torch = []
-    frame_torch = torch.tensor(img).permute(2, 0, 1).float() / 255
-    frame_torch = torchvision.transforms.functional.resize(
-        frame_torch, image_size, antialias=True
-    )
-    frames_torch.append(frame_torch)
-    frames_torch = torch.cat(frames_torch, dim=0).unsqueeze(0)
-    pred = model(frames_torch)
-    pred = pred[0, :, :, :].detach().cpu().numpy()
+    rgb_image = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    image = torchvision.transforms.ToTensor()(rgb_image)
+    image = torchvision.transforms.Resize(size=image_size, antialias=True)(image)
+    image = image.type(torch.float32)
+    image = image.unsqueeze(0)
 
-    pred_frame = pred[0, :, :]
+    pred = model(image)
+    pred_frame = pred[0, 0]
+    pred_frame = pred_frame.detach().numpy()
+
     y, x = np.where(pred_frame == np.max(pred_frame))
     x, y = x[0], y[0]
-    center = (int(x / image_size[1] * w), int(y / image_size[0] * h))
 
-    cv2.circle(img, center, 5, (0, 255, 0), 2)
+    h, w = img.shape[:2]
+    center = (int(x / image_size[1] * w), int(y / image_size[0] * h))
+    cv.circle(img, center, 5, (0, 255, 0), 2)
     return img
